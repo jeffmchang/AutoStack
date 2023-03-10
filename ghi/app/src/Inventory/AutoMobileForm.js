2c750797261973376c2fd45880fa3820582eeedf
Jeff Chang <jeffjmc4@gmail.com>
2023-03-09 20:27:12 -0800

    fixed year mad length

--- ghi/app/src/Inventory/AutoMobileForm.js
+++ ghi/app/src/Inventory/AutoMobileForm.js
@@ -90,7 +90,7 @@ function AutoForm() {
                         </div>
                         <div className="form-floating mb-3">
                             <input
-                                maxLength={4}
+                                maxLength="4"
                                 value={year}
                                 onChange={handleYearChange}
                                 placeholder="Year"
